77ab634d500b474bba53d7b9c4cacc916b8bb77f
JGabrielNeto <joao.gabrielsn1997@gmail.com>
2022-07-10 16:56:24 -0300

    feat: creating company class

---
+++ src/Company.ts
@@ -0,0 +1,19 @@
+import { company, address } from 'faker/locale/pt_BR';
+
+export class Company {
+  companyName: string;
+  catchPhrase: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+
+  constructor() {
+    this.companyName = company.companyName();
+    this.catchPhrase = company.catchPhrase();
+    this.location = {
+      lat: parseFloat(address.latitude()),
+      lng: parseFloat(address.longitude()),
+    };
+  }
+}
