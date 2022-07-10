3453a0306c3f6b2b6b831569ac2faa41f145790c
JGabrielNeto <joao.gabrielsn1997@gmail.com>
2022-07-10 17:40:31 -0300

    feat: effectively creating a map inside the app

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 /// <reference types="@types/google.maps" />
-
 import { User } from './User';
 import { Company } from './Company';
 
+const mapDiv = document.getElementById('map') as HTMLElement;
 const user = new User();
 const company = new Company();
 
-console.log(user);
-console.log(company);
+const map = new google.maps.Map(mapDiv, {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
